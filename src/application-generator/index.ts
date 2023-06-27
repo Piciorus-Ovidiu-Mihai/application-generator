350114a077bf11d481a7a59d9b55ba6d6ff5f401
Piciorus Ovidiu-Mihai <60327678+Piciorus-Ovidiu-Mihai@users.noreply.github.com>
2023-06-27 22:40:13 +0300

    finish latest schema

--- src/application-generator/index.ts
+++ src/application-generator/index.ts
@@ -1,13 +1,42 @@
-import { Rule, SchematicContext, Tree, chain } from '@angular-devkit/schematics';
-import { angularProjectGenerator } from '../angular-project-generator';
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  chain,
+} from "@angular-devkit/schematics";
+import { execSync } from "child_process";
 
-
-export function applicationGenerator(_options: any): Rule {
+export function applicationGenerator(options: any): Rule {
+  createBackEnd();
+  if (options.fronEnd === "Ionic") {
+    createFrontEnd("ionic-project-generator");
+  } else {
+    createFrontEnd("angular-project-generator");
+  }
   return (tree: Tree, _context: SchematicContext) => {
-    const rule = chain([
-      angularProjectGenerator(_options),
-
-    ]);
+    const rule = chain([]);
     return rule(tree, _context) as Rule;
   };
+}
+
+function createBackEnd(): void {
+  const command = "schematics .:back-end-architecture";
+
+  try {
+    execSync(command, { stdio: "inherit" });
+    console.log("Ionic Angular application created successfully.");
+  } catch (error) {
+    console.error("Error creating Ionic Angular application:", error);
+  }
+}
+
+function createFrontEnd(type: string): void {
+  const command = `schematics .:${type}`;
+
+  try {
+    execSync(command, { stdio: "inherit" });
+    console.log("Ionic Angular application created successfully.");
+  } catch (error) {
+    console.error("Error creating Ionic Angular application:", error);
+  }
 }
