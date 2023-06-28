eb4890bcb0b6e89400ea79bd1b7e45ef4bc698a5
Piciorus Ovidiu-Mihai <60327678+Piciorus-Ovidiu-Mihai@users.noreply.github.com>
2023-06-28 20:20:06 +0300

    add left nav

--- src/angular-project-generator/files/layout/layout.module.ts
+++ src/angular-project-generator/files/layout/layout.module.ts
@@ -6,11 +6,33 @@ import { CoreRoutingModule } from '../core/core-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
-  declarations: [MainComponent, FooterComponent, NavigationComponent, LeftNavigationComponent],
-  imports: [CommonModule,FormsModule, ReactiveFormsModule, CoreRoutingModule],
+  declarations: [
+    MainComponent,
+    FooterComponent,
+    NavigationComponent,
+    LeftNavigationComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CoreRoutingModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   exports: [MainComponent, FooterComponent, NavigationComponent],
 })
 export class LayoutModule {}
--- src/angular-project-generator/files/layout/left-navigation/left-navigation.component.ts
+++ src/angular-project-generator/files/layout/left-navigation/left-navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { menus } from 'src/constants/menus';
 
 @Component({
   selector: 'app-left-navigation',
@@ -6,9 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./left-navigation.component.scss']
 })
 export class LeftNavigationComponent {
+  public headerMenuButtons = this.removeObjectsByName(menus, [
+    {
+      title: 'Home',
+      ref: '/app/home',
+      icon: 'home'
+    },
+    {
+      title: 'Components',
+      ref: '/app/components',
+      icon: 'dashboard'
+    },
+    {
+      title: 'Directives',
+      ref: '/app/directives',
+      icon: 'build'
+    },
+    {
+      title: 'Pipes',
+      ref: '/app/pipes',
+      icon: 'settings'
+    },
+    {
+      title: 'About us',
+      ref: '/app/about-us',
+      icon: 'people'
+    },
+    {
+      title: 'Login',
+      ref: '/auth/login',
+      icon: 'login'
+    },
+  ]);
   public hideSideNav: boolean = false;
+  public mobileQuery: MediaQueryList;
 
   public toggleSideNav(): void {
     this.hideSideNav = !this.hideSideNav;
+  }
+
+  public removeObjectsByName(names: string[], objects: any[]): any[] {
+    return objects.filter((object) => !names.includes(object.title));
   }
 }
